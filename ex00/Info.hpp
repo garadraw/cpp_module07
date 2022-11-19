d543f293c6a74cf0ccbec5bdfd6b9a2492269555
Nartex <garadraw@gmail.com>
2022-11-19 13:55:00 +0100

    Added ex00 for Testing on MAC

--- ex00/Info.hpp
+++ ex00/Info.hpp
@@ -6,7 +6,7 @@
 /*   By: vsimeono <vsimeono@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 12:32:14 by vsimeono          #+#    #+#             */
-/*   Updated: 2022/11/19 12:32:44 by vsimeono         ###   ########.fr       */
+/*   Updated: 2022/11/19 13:50:41 by vsimeono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,25 @@
 
 #include <iostream>
 
+template< typename T >
+void swap(T &a, T &b)
+{
+	T temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
 
+template< typename T >
+T	min(T &a, T &b)
+{
+	return ( b <= a ? b : a);
+}
+
+template< typename T >
+T max(T &a, T &b)
+{
+	return ( b >= a ? b : a);
+}
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,13 +6,35 @@
 /*   By: vsimeono <vsimeono@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 12:32:13 by vsimeono          #+#    #+#             */
-/*   Updated: 2022/11/19 12:33:07 by vsimeono         ###   ########.fr       */
+/*   Updated: 2022/11/19 13:54:35 by vsimeono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Info.hpp"
 
-int main()
+int	main( void )
 {
-		
+	int a = 2;
+	int b = 3;
+	
+	std::cout << "Original values:  a = " << a << ", b = " << b << std::endl;
+	::swap( a, b );
+	std::cout << "Swap New Values: a = " << a << ", b = " << b << std::endl;
+
+	std::cout << "Original values:  a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "Min Values: a = " << a << ", b = " << b << std::endl;
+
+	std::cout << "Original values:  a = " << a << ", b = " << b << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "Max Values: a = " << a << ", b = " << b << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return 0;
 }
